d90f2e43c2478d1c3ae6ad4591a3a5d89ee5b176
Codexgrey <thecodexgrey@gmail.com>
2021-11-03 09:17:42 +0100

    basic blog functionalities added

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,35 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
+
 # Create your views here.
+    #NOTE - post_list as function-based view
 def post_list(request):
-    posts = Post.published.all()
-    context = {'posts': posts}
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'page': page, 'posts': posts}
     return render(request, 'blog/post/list.html', context)
+
+
+    #NOTE - post_list class-based view
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -19,3 +43,5 @@ def post_detail(request, year, month, day, post):
     )
     context = {'post': post}
     return render(request, 'blog/post/detail.html', context)
+
+
